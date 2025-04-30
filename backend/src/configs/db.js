3bfc63334116f273ec92156f1ed2143d94b11a30
user <user@users-MacBook-Pro.local>
2025-04-30 20:41:41 +0530

    kuberbeth

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -2,8 +2,8 @@ const mongoose=require("mongoose")
 require("dotenv").config()
 const MONGO_HOST=process.env.MONGO_HOST||'localhost'
 const MONGO_PORT=process.env.MONGO_PORT||'27017'
-const DB=process.env.MONGO_URL||`mongodb://${MONGO_HOST}:${MONGO_PORT}/hacking-point`
-
+const DB=`mongodb://${MONGO_HOST}:${MONGO_PORT}/hacking-point`
+// const DB1 = process.env.MONGO_URL || "mongodb://localhost:27017/hacking-point"
 module.exports=async ()=>{
     try{
         await mongoose.connect(DB)
